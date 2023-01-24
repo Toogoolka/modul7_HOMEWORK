d52c0dae709268464ad063f906b6907c8a43a79c
Toogoolka <120179327+Toogoolka@users.noreply.github.com>
2023-01-25 00:45:28 +0300

    4 coompleted

--- 4.js
+++ 4.js
@@ -1,49 +1,41 @@
-let counterPower = 0;
-let deviceCondition = 0;
+let options = {
+    counterPower: 0,
+    nameDevice1: "Светильник",
+    nameDevice2: "Чайник",
+    device1Power: 115,
+    device2Power: 1150,
+}
 
-let device1Power = 115;
-let device2Power = 1150;
-function Devices(condition, power){
+function Devices(power, name){
     this.deviceIs = "Electric device",
-    this.turnOn = function (condition){
-        if (condition === 0){
-            console.log("===Включён===");
-            deviceCondition = 1;
-        }
+    this.turnOn = function (name, power){
+        console.log(`\nподключил ${name} к сети`);
+        options["counterPower"] += power;
+        console.log(`Актуальная нагрузка на сеть: ${options["counterPower"]} Ватт/час.`)
     }
-    this.turnOff = function (condition){
-        if(condition === 1){
-            console.log("===Выключен===");
-            deviceCondition = 0;
-        }
-    }
-}
-Devices.prototype.getPower = function (condition, power) {
-    if (condition === 0) {
-        counterPower += 0;
-    } else if (condition === 1) {
-        counterPower += power;
-    } else {
-        console.log("Прибор может быть только в состоянии 1-включен и 0-выключен")
+
+    this.turnOff = function (name, power){
+        console.log(`\n=== Потребитель ${name} отсоединен от сети ===`);
+        options["counterPower"] -= power;
+        console.log(`Актуальная нагрузка на сеть: ${options["counterPower"]} Ватт/час.`)
     }
 }
 
-function WhichDevice(condition, power, name){
-    this.name = name,
-    this.pluged = condition
+function WhichDevice(power, name){
+    this.name = name
 }
 WhichDevice.prototype = new Devices();
 
-const lamp = new WhichDevice(deviceCondition, device1Power, "Table Lamp");
-console.log(lamp);
-const computer = new WhichDevice(deviceCondition, device2Power, "My computer");
-console.log(computer);
+const lamp = new WhichDevice(options["device1Power"], options["nameDevice1"]);
+const computer = new WhichDevice(options["device2Power"], options["nameDevice2"]);
+console.log(`\nНагрузка на сеть: ${options["counterPower"]} Ватт/час...`)
 
-lamp.turnOn(deviceCondition);
-lamp.getPower(deviceCondition, 115);
-computer.turnOn(deviceCondition)
-computer.getPower(deviceCondition, 1150)
+lamp.turnOn(options["nameDevice1"], options["device1Power"]);
+computer.turnOn(options["nameDevice2"], options["device2Power"])
+lamp.turnOff(options["nameDevice1"], options["device1Power"]);
 
-console.log(`Общая потребляемая мощность приборов: ${counterPower}`)
+
+console.log(`\n********************************
+Сеть нагружена на: ${options["counterPower"]} Ватт/час`)
 
 
